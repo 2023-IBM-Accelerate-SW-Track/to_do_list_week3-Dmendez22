7c5aa7b389647bccf3e7e3183c922f821623d39d
Danny Mendez <dmendezvm@gmail.com>
2023-06-25 14:32:05 -0700

    Created test that app doesn't render duplicate task

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -19,25 +19,39 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', { name: /Add New Item/i });
+  const inputDate = screen.getByPlaceholderText('MM/DD/YYYY');
+  const element = screen.getByRole('button', { name: /Add/i });
 
- test('test that App component doesn\'t add a task without task name', () => {
+  fireEvent.change(inputTask, { target: { value: 'History Test' } });
+  fireEvent.change(inputDate, { target: { value: '05/30/2023' } });
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, { target: { value: 'History Test' } });
+  fireEvent.click(element);
+
+  const tasks = screen.getAllByText('History Test');
+  expect(tasks.length).toBe(1);
+});
+
+test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
 
- test('test that App component doesn\'t add a task without due date', () => {
+});
+
+test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
- });
+});
 
 
 
- test('test that App component can be deleted thru checkbox', () => {
+test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
- });
+});
 
 
- test('test that App component renders different colors for past due events', () => {
+test('test that App component renders different colors for past due events', () => {
   render(<App />);
- });
+});
